0bef7e7cee840a87fe73dd6d14fe626d7f4ff9c7
ProfMariaClara <129232555+ProfMariaClara@users.noreply.github.com>
2023-10-11 15:22:25 -0300

    teclasom

--- main.js
+++ main.js
@@ -1,39 +1,10 @@
-function tocaSom (seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
-
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
-
+function tocaSomGrilo () {
+    document.querySelector("#som_tecla_grilo").play ();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-    tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        }
-
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-
+function tocaSomAplausos () {
+    document.querySelector("#som_tecla_aplausos").play ();
 }
+document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;
